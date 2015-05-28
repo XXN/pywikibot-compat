73ffe9bebe17cdcc33df338b43188f0cb42da48c
xqt <info@gno.de>
2015-05-28 15:33:00 +0200

    update mw and wm versions
    Change-Id: I4374aec3c87fd1f9bb2757481dd42c7ba00159e6

--- family.py
+++ family.py
@@ -4331,7 +4331,7 @@ class Family(object):
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
         # Here we return the latest mw release for downloading
-        return '1.24.2'
+        return '1.25.1'
 
     def versionnumber(self, code, version=None):
         """Return an int identifying MediaWiki version.
@@ -4967,7 +4967,7 @@ class WikimediaFamily(Family):
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
         # Here we return the latest mw release of wikimedia projects
-        return '1.26wmf5'
+        return '1.26wmf7'
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
